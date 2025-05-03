a7de43cc4fedecf48bd25cec24a055c0c5cdfef2
Leonardo Sarto <leonardosarto1@gmail.com>
2025-05-03 08:39:02 -0300

    Changing schema prisma and appointment.service

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -57,7 +57,6 @@ export class AppointmentService {
         ...(client && {
           client: {
             contains: client,
-            mode: 'insensitive',
           },
         }),
         ...(period && {
